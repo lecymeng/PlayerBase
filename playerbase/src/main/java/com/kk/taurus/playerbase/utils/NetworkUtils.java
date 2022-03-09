78e6412497e1e9b06a17ecd7f50eedeb193f51ba
Weiwei <weiwei.me@foxmail.com>
2022-03-09 14:22:48 +0800

    移除移动网络类型判断，只需要知道是移动网络即可

--- playerbase/src/main/java/com/kk/taurus/playerbase/utils/NetworkUtils.java
+++ playerbase/src/main/java/com/kk/taurus/playerbase/utils/NetworkUtils.java
@@ -20,13 +20,9 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.telephony.TelephonyManager;
 
-import static com.kk.taurus.playerbase.config.PConst.NETWORK_STATE_2G;
-import static com.kk.taurus.playerbase.config.PConst.NETWORK_STATE_3G;
-import static com.kk.taurus.playerbase.config.PConst.NETWORK_STATE_4G;
 import static com.kk.taurus.playerbase.config.PConst.NETWORK_STATE_CONNECTING;
-import static com.kk.taurus.playerbase.config.PConst.NETWORK_STATE_MOBILE_UNKNOWN;
+import static com.kk.taurus.playerbase.config.PConst.NETWORK_STATE_MOBILE;
 import static com.kk.taurus.playerbase.config.PConst.NETWORK_STATE_NONE;
 import static com.kk.taurus.playerbase.config.PConst.NETWORK_STATE_WIFI;
 
@@ -59,8 +55,8 @@ public class NetworkUtils {
                 return NETWORK_STATE_NONE;
             }
         }
-        // is wifi ?
-        @SuppressLint("MissingPermission")
+
+        @SuppressLint("MissingPermission") // require android.permission.ACCESS_NETWORK_STATE
         NetworkInfo wifiInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
         if (null != wifiInfo) {
             NetworkInfo.State state = wifiInfo.getState();
@@ -70,51 +66,8 @@ public class NetworkUtils {
                 }
             }
         }
-        // 2G、3G、4G ?
-        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        int networkType = telephonyManager.getNetworkType();
-        switch (networkType) {
-            /*
-             GPRS : 2G(2.5) General Packet Radia Service 114kbps
-             EDGE : 2G(2.75G) Enhanced Data Rate for GSM Evolution 384kbps
-             UMTS : 3G WCDMA 联通3G Universal Mobile Telecommunication System 完整的3G移动通信技术标准
-             CDMA : 2G 电信 Code Division Multiple Access 码分多址
-             EVDO_0 : 3G (EVDO 全程 CDMA2000 1xEV-DO) Evolution - Data Only (Data Optimized) 153.6kps - 2.4mbps 属于3G
-             EVDO_A : 3G 1.8mbps - 3.1mbps 属于3G过渡，3.5G
-             1xRTT : 2G CDMA2000 1xRTT (RTT - 无线电传输技术) 144kbps 2G的过渡,
-             HSDPA : 3.5G 高速下行分组接入 3.5G WCDMA High Speed Downlink Packet Access 14.4mbps
-             HSUPA : 3.5G High Speed Uplink Packet Access 高速上行链路分组接入 1.4 - 5.8 mbps
-             HSPA : 3G (分HSDPA,HSUPA) High Speed Packet Access
-             IDEN : 2G Integrated Dispatch Enhanced Networks 集成数字增强型网络 （属于2G，来自维基百科）
-             EVDO_B : 3G EV-DO Rev.B 14.7Mbps 下行 3.5G
-             LTE : 4G Long Term Evolution FDD-LTE 和 TDD-LTE , 3G过渡，升级版 LTE Advanced 才是4G
-             EHRPD : 3G CDMA2000向LTE 4G的中间产物 Evolved High Rate Packet Data HRPD的升级
-             HSPAP : 3G HSPAP 比 HSDPA 快些
-             */
-            // 2G
-            case TelephonyManager.NETWORK_TYPE_GPRS:
-            case TelephonyManager.NETWORK_TYPE_CDMA:
-            case TelephonyManager.NETWORK_TYPE_EDGE:
-            case TelephonyManager.NETWORK_TYPE_1xRTT:
-            case TelephonyManager.NETWORK_TYPE_IDEN:
-                return NETWORK_STATE_2G;
-            // 3G
-            case TelephonyManager.NETWORK_TYPE_EVDO_A:
-            case TelephonyManager.NETWORK_TYPE_UMTS:
-            case TelephonyManager.NETWORK_TYPE_EVDO_0:
-            case TelephonyManager.NETWORK_TYPE_HSDPA:
-            case TelephonyManager.NETWORK_TYPE_HSUPA:
-            case TelephonyManager.NETWORK_TYPE_HSPA:
-            case TelephonyManager.NETWORK_TYPE_EVDO_B:
-            case TelephonyManager.NETWORK_TYPE_EHRPD:
-            case TelephonyManager.NETWORK_TYPE_HSPAP:
-                return NETWORK_STATE_3G;
-            // 4G
-            case TelephonyManager.NETWORK_TYPE_LTE:
-                return NETWORK_STATE_4G;
-            default:
-                return NETWORK_STATE_MOBILE_UNKNOWN;
-        }
+
+        return NETWORK_STATE_MOBILE;
     }
 
     public static boolean isMobile(int networkState){
